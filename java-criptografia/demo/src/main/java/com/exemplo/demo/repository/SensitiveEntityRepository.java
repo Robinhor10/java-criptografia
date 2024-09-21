af61d06df42e152fdc6e281d4b3dfb72dcd7c459
Robinhor10 <49103782+Robinhor10@users.noreply.github.com>
2024-09-21 15:36:48 -0300

    Delete java-criptografia/demo directory

--- java-criptografia/demo/src/main/java/com/exemplo/demo/repository/SensitiveEntityRepository.java
+++
@@ -1,8 +0,0 @@
-package com.exemplo.demo.repository;
-
-import com.exemplo.demo.model.SensitiveEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SensitiveEntityRepository extends JpaRepository<SensitiveEntity, Long> {
-
-}
